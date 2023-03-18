1aae0db8a02969ab5a66ff94a1b2c58ecaa98b9a
rzkystwn0 <rzkystwn0@gmail.com>
2023-03-18 22:39:53 +0700

    ffrist

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet"/>
+      </Head>
+      <body className='bg-[#ef6179]'>
         <Main />
         <NextScript />
       </body>
